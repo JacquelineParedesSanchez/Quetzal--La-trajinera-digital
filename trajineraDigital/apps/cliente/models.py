fb503398611404522c824c92c2b5c039b15b9648
Fernando <jfaltamiranov@ciencias.unam.mx>
2020-05-13 14:37:54 -0500

    requirements, client

--- trajineraDigital/apps/cliente/models.py
+++ trajineraDigital/apps/cliente/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 from apps.menu.models import Orden
 # Create your models here.
 class Cliente(models.Model):
-	
+
 	user = models.OneToOneField(
 		User, related_name="client", on_delete=models.CASCADE
 	)
 	direccion = models.TextField()
 	telefono = models.CharField(max_length=10)
-	ordenes = models.ManyToOneField(Orden, blank=True)
+#	ordenes = models.ManyToOneField(Orden, blank=True)
 
-	def __str__(self):
-		return f"Usuario({self.user.id} {self.user.first_name}
-			{self.user.last_name} {self.user.email})"
+#	def __str__(self):
+#		return f"Usuario({self.user.id} {self.user.first_name}
+#			{self.user.last_name} {self.user.email})"
 
 	def __repr__(self):
 		return self.__str__()
--- trajineraDigital/apps/cliente/views.py
+++ trajineraDigital/apps/cliente/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from users.
+#from users.
 
 # Create your views here.
 #class SignUpView(View):
+
+def home_view(*args, **kwargs):
+    return HttpResponse("<h1> Bienvenido </h1>")
