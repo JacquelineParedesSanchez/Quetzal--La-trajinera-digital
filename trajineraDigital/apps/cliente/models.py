22d7a5b680ced90606af368f3a89a9f0bed8677e
Fernando <jfaltamiranov@ciencias.unam.mx>
2020-05-15 22:22:44 -0500

    Ingreso manual de usuarios

--- trajineraDigital/apps/cliente/models.py
+++ trajineraDigital/apps/cliente/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 from apps.menu.models import Orden
 # Create your models here.
-class Cliente(models.Model):
+class UserCliente(models.Model):
 
 	user = models.OneToOneField(
 		User, related_name="client", on_delete=models.CASCADE
 	)
-	direccion = models.TextField()
-	telefono = models.CharField(max_length=10)
+	direccion = models.TextField(default='')
+	telefono = models.IntegerField(default=0)
 #	ordenes = models.ManyToOneField(Orden, blank=True)
 
 #	def __str__(self):
 #		return f"Usuario({self.user.id} {self.user.first_name}
 #			{self.user.last_name} {self.user.email})"
 
-	def __repr__(self):
-		return self.__str__()
+def create_profile(sender,**kwargs):
+	if kwargs['created']:
+		client_profile=UserCliente.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
+
+#	def __repr__(self):
+#		return self.__str__()
