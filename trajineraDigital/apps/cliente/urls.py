abb9695195521a686d5a4fab679ed207f88f6817
Fernando <jfaltamiranov@ciencias.unam.mx>
2020-05-18 13:59:54 -0500

    Logout error

--- trajineraDigital/apps/cliente/urls.py
+++ trajineraDigital/apps/cliente/urls.py
@@ -12,9 +12,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as v
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), {'template_name': 'cliente/login.html'}, name='login'),
+    path('login/', v.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    path('logout/',v.LogoutView.as_view(), {'template_name': 'logout.html'}, name='logout'),
 ]
