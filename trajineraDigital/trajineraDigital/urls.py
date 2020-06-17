2c9c2419fb5440d26c31b4b7af924f21a907acf3
David A. Martinez Figueroa <damf@ciencias.unam.mx>
2020-06-17 13:15:41 -0500

    Cosas mas

--- trajineraDigital/trajineraDigital/urls.py
+++ trajineraDigital/trajineraDigital/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -22,7 +22,8 @@ from trajineraDigital import views as vista
 
 urlpatterns = [
     #path('', vista.login_redirect, name='login_redirect'),
-    path('', vista.bienvenida, name='bienvenida'),
+
+    path('', vista.bienvenida , name='bienvenida'),
     path('admin/', admin.site.urls),
     path('home/', include('apps.cliente.urls')),
     path('administrador/',include('apps.usuarios.urls')),
